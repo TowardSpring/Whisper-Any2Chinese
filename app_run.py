b3159ca15fe9a32a074e7a812590255ea8865224
TowardSpring <10xuxiangchun1995@foxmail.com>
2023-08-09 15:43:38 +0800

    routine

--- app_run.py
+++ app_run.py
@@ -1,19 +1,116 @@
-# whisper model deploy
-
 import gradio as gr
-import os
+from whisper_test import WHISPER_TOOL
+import shutil
 
-# save your HF API token from https:/hf.co/settings/tokens as an env variable to avoid rate limiting
-auth_token = os.getenv("auth_token")
+import librosa
+from datasets import Dataset
+import torch
 
-# automatically load the interface from a HF model 
-# you can remove the api_key parameter if you don't care about rate limiting. 
-demo = gr.load(
-    "huggingface/facebook/wav2vec2-base-960h",
-    title="Speech-to-text",
-    inputs="mic",
-    description="Let me try to guess what you're saying!",
-    hf_token=auth_token
-)
+def run_whisper(input_language,task,record):
 
-demo.launch()
+    if input_language == 'Uyghur/维吾尔语':
+        input_language = 'ug'
+    elif input_language == 'English/英语':
+        input_language = 'en'
+    elif input_language == 'Chinese/中文':
+        input_language = 'zh'
+
+    
+    if task == 'Transcribe/转录':
+        task = 'transcribe'
+    elif task == 'Any to English/转录+翻译成英语':
+        task = 'translate'
+    elif task == 'Any to Chinese/转录+翻译中文':
+        task = 'translate-chinese'
+
+    text  = '未能实现转录。'
+    print(f'input_language:{input_language},task:{task},record:{record}')
+    print(f'type of input_language:{type(input_language)},type of task:{type(task)},type of record:{type(record)}')
+
+    whisper_tool = WHISPER_TOOL()
+    # 检查record是否为文件路径
+    if isinstance(record,str):
+        # 保存文件到./data/online/temp_audio.mp3
+        shutil.copy(record,'./data/online/temp_audio.mp3')
+        record_file = './data/online/temp_audio.mp3'
+        audio_data, sample_rate = librosa.load(record_file)
+
+
+        data = {'audio':[audio_data],'sampling_rate':[sample_rate]}
+        dataset = Dataset.from_dict(data)
+        text = whisper_tool.run_whisper(input_language,task,dataset)
+        print("text:",text)
+
+    # 检查是否是numpy类型
+    elif isinstance(record,tuple):
+        sample_rate = record[0]
+        data = {'audio':[torch.from_numpy(record[-1])],'sampling_rate':[sample_rate]}
+        dataset = Dataset.from_dict(data)
+        text = whisper_tool.run_whisper(input_language,task,dataset)
+        print("text:",text)
+    if type(text) == list:
+        text = '\n'.join(text)
+        return text
+    
+    else:
+        return text
+
+with gr.Blocks() as demo:    
+    gr.Markdown("# <center>Whisper - Any to Chinese</center>")
+    gr.Markdown("### Please select the upload method for the audio/请选择音频上传方式")
+
+    with gr.Tab('上传音频文件'):
+        with gr.Row(): #并行显示，可开多列
+            with gr.Column(): # 并列显示，可开多行
+                gr.Markdown("### Please select the input language/请选择输入语种")
+                drop1 = gr.Dropdown(["Uyghur/维吾尔语", "English/英语", "Chinese/中文"], 
+                label="Input Languages/输入语种", info="(Will add more languages later!/陆续添加更多语种！)") #单选
+
+                gr.Markdown("### Please select the task type/请选择任务类型")
+                task = gr.Dropdown(["Transcribe/转录","Any to English/转录+翻译成英语", "Any to Chinese/转录+翻译中文"], 
+                label="Task Type/任务类型", info="(Will add more task types!/陆续添加更多任务类型！)") #单选
+
+                gr.Markdown("### Plese click the button to start recording/请点击开始录音")
+                record1=gr.Audio(type="filepath")
+                bottom2 = gr.Button(label="demo2")
+
+            outputs=gr.Textbox(label='ASR Output/语音自动识别输出')
+
+            
+
+        bottom2.click(run_whisper, inputs=[drop1, task, record1], outputs=outputs) # 触发
+    
+    with gr.Tab('麦克风录音'):
+        with gr.Row(): #并行显示，可开多列
+            with gr.Column(): # 并列显示，可开多行
+                gr.Markdown("### Please select the input language/请选择输入语种")
+                drop1 = gr.Dropdown(["Uyghur/维吾尔语", "English/英语", "Chinese/中文"], 
+                label="Input Languages/输入语种", info="(Will add more languages later!/陆续添加更多语种！)") #单选
+
+                gr.Markdown("### Please select the task type/请选择任务类型")
+                task = gr.Dropdown(["Transcribe/转录","Any to English/转录+翻译成英语", "Any to Chinese/转录+翻译中文"], 
+                label="Task Type/任务类型", info="(Will add more task types!/陆续添加更多任务类型！)") #单选
+
+                gr.Markdown("### Plese click the button to start recording/请点击开始录音")
+                record1 = gr.Microphone(source="microphone") #录音
+
+                bottom1 = gr.Button(label="demo1")
+
+            out1 = gr.Textbox(label='ASR Output/语音自动识别输出')
+        bottom1.click(run_whisper, inputs=[drop1, task, record1], outputs=out1) # 触发
+
+    
+
+
+
+
+
+
+
+demo.launch(share=True,server_port=6672) # 启动
+
+
+
+
+
+# PID: , nohup python3 app_run.py > ./log/app_run.log 2>&1 &
---
+++ whisper_test.py
@@ -0,0 +1,126 @@
+# test whsiper model
+
+from huggingface_hub import notebook_login
+from datasets import load_dataset, DatasetDict, Dataset
+from pprint import pprint
+from transformers import WhisperFeatureExtractor
+import os
+from transformers import WhisperTokenizer
+from transformers import WhisperProcessor
+
+from datasets import Audio
+import torch
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+import pandas as pd
+import evaluate
+from transformers import WhisperForConditionalGeneration,Seq2SeqTrainingArguments
+from peft import prepare_model_for_int8_training,LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model,PeftModel, PeftConfig
+from transformers import Seq2SeqTrainer, TrainerCallback, TrainingArguments, TrainerState, TrainerControl,WhisperForConditionalGeneration, Seq2SeqTrainer
+from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+import librosa
+import gc
+import numpy as np
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from transformers.models.whisper.english_normalizer import BasicTextNormalizer
+
+class WHISPER_TOOL():
+    def __init__(self):
+        # 参数配置
+        model_name_or_path = "openai/whisper-large-v2"
+        task = "translate-chinese"
+
+        dataset_name = "mozilla-foundation/common_voice_11_0"
+        language = "uyghur"
+        language_abbr = "ug" # Short hand code for the language we want to fine-tune
+
+        # 加载模型
+
+        self.feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name_or_path)
+        self.tokenizer = WhisperTokenizer.from_pretrained(model_name_or_path, language=language, task=task)
+        self.processor = WhisperProcessor.from_pretrained(model_name_or_path, language=language, task=task)
+
+        # 评估
+        peft_model_id = "reach-vb/whisper-large-v2-uygur-1000steps-100epochs" # Use the same model ID as before.
+        peft_config = PeftConfig.from_pretrained(peft_model_id)
+        
+        self.model = WhisperForConditionalGeneration.from_pretrained(
+            peft_config.base_model_name_or_path, load_in_8bit=True, device_map="auto"
+        )
+        self.model = PeftModel.from_pretrained(self.model, peft_model_id)
+        self.model.config.use_cache = True
+
+    def prepare_dataset(self,batch):
+        # load and resample audio data from 48 to 16kHz
+        audio = batch["audio"]
+
+        # compute log-Mel input features from input audio array
+        batch["input_features"] = self.feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+
+        # encode target text to label ids
+        batch["labels"] = self.tokenizer(batch["sentence"]).input_ids
+        return batch
+
+    def run_whisper(self, input_language,task,record):
+        target_rate = 16000
+        data_item = self.feature_extractor(record[0]["audio"], sampling_rate=target_rate).input_features[0]
+        # data_label = self.tokenizer(record[0]["sentence"]).input_ids
+        # record = record.map(self.prepare_dataset, num_proc=12)
+        forced_decoder_ids = self.processor.get_decoder_prompt_ids(language=input_language, task=task)
+        # normalizer = BasicTextNormalizer()
+
+        self.model.eval()
+
+        with torch.cuda.amp.autocast():
+            with torch.no_grad():
+                generated_tokens = (
+                    self.model.generate(
+                        input_features=torch.from_numpy(data_item).unsqueeze(0).to('cuda'),
+                        forced_decoder_ids=forced_decoder_ids,
+                        max_new_tokens=255,
+                    )
+                    .cpu()
+                    .numpy()
+                )
+                # labels = data_label.cpu().numpy()
+                # labels = np.where(labels != -100, labels, self.processor.tokenizer.pad_token_id)
+                decoded_preds = self.processor.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+                # decoded_labels = self.processor.tokenizer.batch_decode(data_label, skip_special_tokens=True)
+
+                # print(f'decoded_preds: {decoded_preds}, decoded_labels: {record[0]["sentence"]}')
+                # 将预测结果保存到'/home/towardspring/projects/asr/faster_whisper_fineturning/data/pred.txt'文件中
+                # with open('/home/towardspring/projects/asr/faster_whisper_fineturning/data/pred.txt', 'w') as f:
+                #     f.write(decoded_preds)
+
+                del generated_tokens
+            gc.collect()
+
+        return decoded_preds
+
+if __name__ == "__main__":
+    whisper_tool = WHISPER_TOOL()
+    input_language = "uyghur"
+    # target_sample_rate = 16000
+    task = "transcribe"
+    # 导入common_voice_ug_26049139.mp3
+    
+    
+    file_path = "/home/towardspring/projects/asr/faster_whisper_fineturning/data/common.mp3"  # 替换为你的MP3文件路径
+
+    # 导入MP3文件并进行采样
+    audio_data, sample_rate = librosa.load(file_path)
+
+
+    data = {'audio':[audio_data],'sampling_rate':[sample_rate],'sentence':["ئۇ ئۆمۈر بويى ئۆز خىزمىتىگە سادىق بولۇپ، ئادىمىيلىك بۇرچىنى ئادا قىلىپ كەلگەنىدى."]}
+    dataset = Dataset.from_dict(data)
+    print(data)
+    print("数据集大小:", len(dataset))
+    # print("示例数据:", dataset)
+
+
+    # print("shape of audio_data:",dataset.shape)
+    # 从MP3文件中提取文本
+    text = whisper_tool.run_whisper(input_language,task,dataset)
+    print("text:",text)
