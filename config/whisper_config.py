0c2299307e0b7dccde7c0515889aefe9d7dc18d7
TowardSpring <10xuxiangchun1995@foxmail.com>
2023-09-25 20:52:15 +0800

    routine

--- config/whisper_config.py
+++ config/whisper_config.py
@@ -1,6 +1,6 @@
 # Whisper Config
 # You should change the following config parameters according to your own environment
-MODEL_CONFIG_FILE_ROOT_PATH = '/Users/towardspring/.cache/huggingface/hub/models--openai--whisper-small/snapshots/e34e8ae444c29815eca53e11383ea13b2e362eb0'
+MODEL_CONFIG_FILE_ROOT_PATH = '/home/towardspring/.cache/huggingface/hub/models--openai--whisper-small/snapshots/e34e8ae444c29815eca53e11383ea13b2e362eb0'
 
 DATASET_NAME_OR_PATH = "./data/en_mini"
 LANGUAGE = "english"
