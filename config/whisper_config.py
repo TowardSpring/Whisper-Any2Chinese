01afa434071a675a5cfa0b40f1c68f3e2f845668
TowardSpring <10xuxiangchun1995@foxmail.com>
2023-09-26 13:17:31 +0800

    routine

--- config/whisper_config.py
+++ config/whisper_config.py
@@ -6,7 +6,7 @@ DATASET_NAME_OR_PATH = "./data/en_mini"
 LANGUAGE = "english"
 LANGUAGE_ABBR = "en" 
 
-MOEDL_NAME_OR_PATH = "openai/whisper-small"
+MOEDL_NAME_OR_PATH = "/home/towardspring/.cache/huggingface/hub/models--openai--whisper-small/snapshots/e34e8ae444c29815eca53e11383ea13b2e362eb0"
 TASK = "translate-chinese"
 NEW_TOKENS = ['<|translate-chinese|>','<|en|>']
 
